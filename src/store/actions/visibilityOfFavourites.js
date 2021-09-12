07475565833ab29b66fa4c9694db106d618a1ea0
AlexandrMisiuk <alexandr.misiuk@gmail.com>
2021-09-12 19:59:20 +0300

    remove favorites visibility toggle from global store

--- src/store/actions/visibilityOfFavourites.js
+++
@@ -1,9 +0,0 @@
-export const VISIBILITY_OF_FAVOURITES = "favourites/VISIBILITY_OF_FAVOURITES";
-
-// change the visibility of favorites
-export const changeFavouritesVisibility = (boolen) => {
-  return {
-    type: VISIBILITY_OF_FAVOURITES,
-    payload: boolen
-  };
-};
